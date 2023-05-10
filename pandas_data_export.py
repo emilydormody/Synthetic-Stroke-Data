92f8ca0da1dbea34ff4d6d476de84ab8d3491efd
emoly <emdormody@mun.ca>
2023-05-10 10:14:03 -0230

    pandas dataframe

--- pandas_data_export.py
+++ pandas_data_export.py
@@ -7,7 +7,9 @@ def main():
     for i in range(2880):  # 48 hours of steps
         h.step()
 
-    d = {"Arrival Time": track_arrivals(h), "ICU Arrival Time": track_icu_arrival(h)}
+    d = {"Patient Id": patient_name(h), "Age": patient_age(h), "Gender": patient_gender(h), "Arrival Time": track_arrivals(h),
+         "CT Scan Time": track_ctscans(h), "Thrombolysis Treatment Time": track_treatment(h),"ICU Arrival Time": track_icu_arrival(h),
+         "Neurologist Visit Time": track_neurologist(h)}
     df = pd.DataFrame(data=d)
     print(df.head())
 
