acc3f4268116e3f001860af959ba52d3815ed914
emoly <emdormody@mun.ca>
2023-07-13 11:35:04 -0230

    none

--- basic_model/scheduler.py
+++ basic_model/scheduler.py
@@ -11,10 +11,10 @@ class NeuroScheduler:
         try:
             if len(self.times_lst) <= 0:
                 return None
-            while self.times_lst[0][1] < current - 30:
-                self.times_lst.pop(0)
-                if len(self.times_lst) <= 0:
-                    return None
+            # while self.times_lst[0][1] < current - 30:
+            #     self.times_lst.pop(0)
+            #     if len(self.times_lst) <= 0:
+            #         return None
             self.next_time_name = self.times_lst[0][0]
             return self.times_lst[0][1]
         except IndexError:
@@ -28,9 +28,9 @@ class NeuroScheduler:
     def add_value(self, value):
         if type(value) is tuple:
             self.times_lst.append(value)
-            i = len(self.times_lst)-2
+            i = len(self.times_lst) - 2
             while self.times_lst[i][1] > value[1]:
-                self.times_lst[i+1] = self.times_lst[i]
+                self.times_lst[i + 1] = self.times_lst[i]
                 self.times_lst[i] = value
                 i -= 1
                 if i < 0:
@@ -38,18 +38,23 @@ class NeuroScheduler:
 
     def filter_times(self):
         i = 0
-        while i < len(self.times_lst)-1:
-            while self.times_lst[i][1] + 30 > self.times_lst[i+1][1]:
-                self.times_lst.remove(self.times_lst[i+1])
-                if i == len(self.times_lst)-1:
+        while i < len(self.times_lst) - 1:
+            while self.times_lst[i][1] + 30 > self.times_lst[i + 1][1]:
+                self.times_lst.remove(self.times_lst[i + 1])
+                if i == len(self.times_lst) - 1:
                     break
             i += 1
-        print(self.patent,self.times_lst)
+        print(self.patent, self.times_lst)
 
+    def drop_values(self, current):
+        while self.times_lst[0][1] < current - 30:
+            self.times_lst.pop(0)
+            if len(self.times_lst) <= 0:
+                return
 
 
 s = NeuroScheduler(5)
-s.add_value(('one',10))
+s.add_value(('one', 10))
 s.add_value(('six', 60))
 s.add_value(('two', 20))
 s.add_value(('eight', 30))
